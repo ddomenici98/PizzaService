e4a291e498d8851d976d42bfb353233d1f832d21
dario <dario.pesce46@gmail.com>
2023-12-18 01:41:26 +0100

    andrea ghei

--- src/main/java/it/poggio/software/restful/PizzaController.java
+++ src/main/java/it/poggio/software/restful/PizzaController.java
@@ -2,9 +2,17 @@ package it.poggio.software.restful;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @Slf4j
+@RequestMapping("pizza")
 public class PizzaController {
 
+    @GetMapping(value = "/{id}")
+    public void getPizza(@PathVariable String idPizza) {
+
+    }
 }
