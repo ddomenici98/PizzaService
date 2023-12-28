9ae7208ba9104085f717f2641e15b39923b9f62c
ddomenici98 <domenicidaniele00@gmail.com>
2023-12-28 22:30:14 +0100

    Commit mapper brutti senza alias(almeno ho messo i commenti dove vanno usati)

--- src/main/java/it/poggio/software/dao/mapper/PizzaMapper.java
+++ src/main/java/it/poggio/software/dao/mapper/PizzaMapper.java
@@ -11,7 +11,7 @@ public class PizzaMapper implements RowMapper<Pizza> {
     public Pizza mapRow(ResultSet rs, int rowNum) throws SQLException {
         return Pizza.builder()
                 .id(rs.getInt("id"))
-                .name(rs.getString("nome"))
+                .name(rs.getString("nome")) // usare alias
                 .price(rs.getDouble("prezzo"))
                 .description(rs.getString("descrizione"))
                 .categoryName(rs.getString("nomeCategoria"))
